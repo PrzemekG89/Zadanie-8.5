0183cb3ae23ec5ac76b4e7943c777190c2934811
Przemek Gałdys <galdys.przemek@gmail.com>
2017-10-25 17:29:08 +0200

    Zadanie 8.5

---
+++ scripts.js
@@ -0,0 +1,16 @@
+//pole trójkąta 1
+
+var a = 5,
+    h = 7,
+    triangleArea = a * h / 2
+
+console.log('tringle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+//pole trójkąta 2
+
+var a2 = prompt('enter a2 value'),
+    h2 = prompt('ender h2 value'),
+    triangleArea2 = a2 * h2 / 2
+
+alert('triangle2 field with base a2: ' + a2 + ' and height h2: ' + h2 + ' is equal to: ' + triangleArea2)
+console.log('triangle2 field with base a2: ' + a2 + ' and height h2: ' + h2 + ' is equal to: ' + triangleArea2)
